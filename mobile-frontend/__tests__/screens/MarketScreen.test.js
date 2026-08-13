973b12a735e82d24e91f7e57be33969b4d35e3e8
Abrar Ahmed <abrarahmedny@gmail.com>
2026-08-13 15:59:18 +0500

    Refactor code and enhance maintainability.

--- mobile-frontend/__tests__/screens/MarketScreen.test.js
+++ mobile-frontend/__tests__/screens/MarketScreen.test.js
@@ -30,13 +30,7 @@ jest.mock(
 const renderMarketScreen = () => {
   // Add Context Providers if needed
   return render(
-    <MarketScreen
-      navigation={
-        {
-          /* mock navigation if needed */
-        }
-      }
-    />,
+    <MarketScreen navigation={{/* mock navigation if needed */}} />,
   );
 };
 
